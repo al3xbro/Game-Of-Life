21cb2ca220b1198719e07ce411469c4af3023de3
al3xbro <alexnbui@gmail.com>
2023-01-05 00:56:48 -0800

    Implemented color inheritance and Cell trails

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,12 +1,14 @@
 package main;
 
+import java.awt.Color;
+
 public class Game {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		int[][] premade = new int[][]{{0, 1, 0},
-		           				   	  {0, 1, 0},
-		           				   	  {0, 1, 0}};
+		Cell[][] premade = new Cell[][]{{new Cell(Color.blue, false), new Cell(Color.green, true), new Cell(Color.blue, false)},
+		           				   	  	{new Cell(Color.blue, false), new Cell(Color.blue, true), new Cell(Color.blue, false)},
+		           				   	  	{new Cell(Color.blue, false), new Cell(Color.blue, true), new Cell(Color.blue, false)}};
 		GameBoard board = new GameBoard(premade);
 		while (true) {
 			Thread.sleep(500);
--- src/main/GameBoard.java
+++ src/main/GameBoard.java
@@ -31,42 +31,37 @@ public class GameBoard {
 	 * 
 	 * @param premade: premade 2d array of Cells
 	 */
-	public GameBoard(int[][] premade) { // premade board
+	public GameBoard(Cell[][] premade) { // premade board
 		board = new Cell[premade.length][premade[0].length];
 		for (int row = 0; row < premade.length; row++) {
 			for (int col = 0; col < premade[0].length; col++) {
-				if (premade[row][col] == 1) {
-					board[row][col] = new Cell(Color.WHITE, true);
-				}
-				else {
-					board[row][col] = new Cell(Color.WHITE, false);
-				}
+				board[row][col] = premade[row][col];
 			}
 		}
 		rowCount = premade.length;
 		colCount = premade[0].length;
 	}
 	
-	public void editCell(int x, int y) {
-		board[x][y].setState(true);
+	public void editCell(int cRow, int cCol) {
+		board[cRow][cCol].setState(true);
 	}
 	
 	/**
 	 * finds all live neighbors of a cell
 	 * 
-	 * @param x: x coordinate of cell
-	 * @param y: y coordinate of cell
+	 * @param cRow: row number of cell
+	 * @param cCol: col number of cell
 	 * @return ArrayList<Cell>: ArrayList of all live neighbors
 	 */
-	private ArrayList<Cell> getNeighbors(int x, int y) {
+	private ArrayList<Cell> getNeighbors(int cRow, int cCol) {
 		ArrayList<Cell> aliveNeighbors = new ArrayList<>();
 		for (int row = -1; row <= 1; row++) {
 			for (int col = -1; col <= 1; col++) {
-				if (((x+row) >= 0 && (x+row) < rowCount) && // checking within xDim
-					((y+col) >= 0 && (y+col) < colCount) && // checking within yDim
+				if (((cRow+row) >= 0 && (cRow+row) < rowCount) && // checking within xDim
+					((cCol+col) >= 0 && (cCol+col) < colCount) && // checking within yDim
 					!(row == 0 && col == 0)) { // checking not original cell
-					if (board[x+row][y+col].getState()) {
-						aliveNeighbors.add(board[x+row][y+col]);
+					if (board[cRow+row][cCol+col].getState()) {
+						aliveNeighbors.add(board[cRow+row][cCol+col]);
 					}
 				}
 			}
@@ -77,28 +72,55 @@ public class GameBoard {
 	/**
 	 * finds the new state and color of a cell
 	 * 
-	 * @param x: x coordinate of cell
-	 * @param y: y coordinate of cell
+	 * @param cRow: row number of cell
+	 * @param cCol: col number of cell
 	 * @return Cell: new cell with updated properties
 	 */
-	private Cell newState(int x, int y) {
-		ArrayList<Cell> aliveNeighbors = getNeighbors(x, y);
+	private Cell newState(int cRow, int cCol) {
+		Color currColor = board[cRow][cCol].getColor();
+		ArrayList<Cell> aliveNeighbors = getNeighbors(cRow, cCol);
+		float[] cellHSB = Color.RGBtoHSB(currColor.getRed(), currColor.getGreen(), currColor.getBlue(), null);
 		
-		if (board[x][y].getState()) {
+		if (board[cRow][cCol].getState()) {
 			if (aliveNeighbors.size() < 2 || aliveNeighbors.size() > 3) { // it just died
-				return new Cell(null, false); // TODO: make it leave a fresh trail
+				return new Cell(Color.getHSBColor(cellHSB[0], cellHSB[1]-30, cellHSB[2]), false);
 			}
 			else { // it stays alive
-				return new Cell(null, true); // TODO: make it change color based on surroundings
+				float[] averageNeighborRGB = {0, 0, 0};
+				for (Cell neighbor : aliveNeighbors) {
+					averageNeighborRGB[0] += neighbor.getColor().getRed();
+					averageNeighborRGB[1] += neighbor.getColor().getGreen();
+					averageNeighborRGB[2] += neighbor.getColor().getBlue();
+				}
+				averageNeighborRGB[0] /= aliveNeighbors.size();
+				averageNeighborRGB[1] /= aliveNeighbors.size();
+				averageNeighborRGB[2] /= aliveNeighbors.size();
+				
+				return new Cell(new Color((averageNeighborRGB[0] + currColor.getRed())/2, (averageNeighborRGB[1] + currColor.getGreen())/2, (averageNeighborRGB[2] + currColor.getBlue())/2), true);
 			}
 		}
 		
 		else {
 			if (aliveNeighbors.size() == 3) { // it just livened
-				return new Cell(null, true); // TODO: make it inherit color
+				float[] averageNeighborRGB = {0, 0, 0};
+				for (Cell neighbor : aliveNeighbors) {
+					averageNeighborRGB[0] += neighbor.getColor().getRed();
+					averageNeighborRGB[1] += neighbor.getColor().getGreen();
+					averageNeighborRGB[2] += neighbor.getColor().getBlue();
+				}
+				averageNeighborRGB[0] /= aliveNeighbors.size();
+				averageNeighborRGB[1] /= aliveNeighbors.size();
+				averageNeighborRGB[2] /= aliveNeighbors.size();
+				return new Cell(new Color(averageNeighborRGB[0], averageNeighborRGB[1], averageNeighborRGB[2]), true);
+				
 			}
 			else { // it stays dead
-				return new Cell(null, false); // TODO: make it slowly lose trail
+				if (cellHSB[1]-10 > 0) {
+					return new Cell(Color.getHSBColor(cellHSB[0], cellHSB[1]-10, cellHSB[2]), false);
+				}
+				else {
+					return new Cell(Color.WHITE, false);
+				}
 			}
 		}
 		
