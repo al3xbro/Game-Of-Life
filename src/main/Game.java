8f1139e51450c10123a8fd16c1b9284e99758d33
al3xbro <alexnbui@gmail.com>
2023-01-15 20:26:23 -0800

    Updated test

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.awt.*;
+
 public class Game {
 
 	public static void main(String[] args) throws InterruptedException {
@@ -10,21 +11,23 @@ public class Game {
 		for (int row = 0; row < b.rowCount; row++) {
 			for (int col = 0; col < b.colCount; col++) {
 				Cell toPaint = board[row][col];
-				//MainFrame.gPanel.paint(new Rectangle());
+				// MainFrame.gPanel.paint(new Rectangle());
 			}
 		}
-		
-		
-		
-		
-		/*Cell[][] premade = new Cell[][]{{new Cell(Color.blue, false), new Cell(Color.green, true), new Cell(Color.blue, false)},
-		           				   	  	{new Cell(Color.blue, false), new Cell(Color.blue, true), new Cell(Color.blue, false)},
-		           				   	  	{new Cell(Color.blue, false), new Cell(Color.blue, true), new Cell(Color.blue, false)}};
-		GameBoard board = new GameBoard(premade);
-		while (true) {
-			Thread.sleep(500);
-			board.printBoard();
-			board.iterate();
-		}*/
+
+		/*
+		 * Cell[][] premade = new Cell[][]{{new Cell(Color.blue, false), new
+		 * Cell(Color.green, true), new Cell(Color.blue, false)},
+		 * {new Cell(Color.blue, false), new Cell(Color.blue, true), new
+		 * Cell(Color.blue, false)},
+		 * {new Cell(Color.blue, false), new Cell(Color.blue, true), new
+		 * Cell(Color.blue, false)}};
+		 * GameBoard board = new GameBoard(premade);
+		 * while (true) {
+		 * Thread.sleep(500);
+		 * board.printBoard();
+		 * board.iterate();
+		 * }
+		 */
 	}
 }
